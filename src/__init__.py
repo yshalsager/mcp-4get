d799d25c72dcce35ff473533f20391fdf4d0e198
yshalsager <ysh-alsager@hotmail.com>
2025-09-26 23:41:32 +0300

    chore: add FourGetClient to module exports

--- src/__init__.py
+++ src/__init__.py
@@ -10,5 +10,6 @@ except PackageNotFoundError:  # pragma: no cover
     __version__ = '0.1.0'
 
 from .server import create_server  # noqa: E402
+from .client import FourGetClient  # noqa: E402
 
-__all__ = ['create_server', '__version__']
+__all__ = ['create_server', 'FourGetClient', '__version__']
